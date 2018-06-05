0f22b53ccce0f36708330133f3f3b9165fda5a89
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2018-06-05 12:42:20 +0900

    Add all controlers
    -  rename `spigot-listener` instead of `spigot-idle`
    -  add `async-runner` for signal-friendly command
    -  add `decisiontab` for decision table
    -  add `event-fsm` for event-driven finite state machine
    -  add `event-timer-ugly` for event notification timer

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -2,12 +2,33 @@
 
 set -euo pipefail
 
+source /usr/local/etc/event.conf
 source /usr/local/etc/spigot.conf
 
-for DIRECTORY in "${SERVER_DIR}" "${BACKUP_DIR}" "${RESTORE_DIR}"
+DIRECTORIES=(
+    "${SERVER_DIR}"
+    "${BACKUP_DIR}"
+    "${RESTORE_DIR}"
+    "${EVENT_ROOT}"
+)
+
+FIFOS=(
+    "${EVENT_ROOT}/${EVENT_BUS}"
+    "${EVENT_ROOT}/${SERVICE_BACKUP_CONTROLLER}"
+    "${EVENT_ROOT}/${SERVICE_LIST_CONTROLLER}"
+    "${EVENT_ROOT}/${SERVICE_STOP_CONTROLLER}"
+)
+
+for DIRECTORY in "${DIRECTORIES[@]}"
 do
     mkdir -p "${DIRECTORY}"
     chown "${PUID}:${PGID}" "${DIRECTORY}"
+done
+
+for FIFO in "${FIFOS[@]}"
+do
+    mkfifo -m 600 "${FIFO}"
+    chown "${PUID}:${PGID}" "${FIFO}"
 done
 
 if [ -n "${EULA:-}" ]
